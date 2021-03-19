8310020782a11243252ad1eb6dd270896a87a4dc
Agnesotti87 <77006752+Agnesotti87@users.noreply.github.com>
2021-03-19 10:45:42 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -6,9 +6,12 @@ setwd("C:/lab/")
 p224r63_2011 <- brick("p224r63_2011_masked.grd")
 #get information
 p224r63_2011 
+#p = path (sinusoide che disegna il passaggio del satellite)
+# r= row (riga orizzontale che interseca le sinusoidi)
+# path e row ci danno il punto in cui "cade" l'immagine
 #and plot it
 plot(p224r63_2011)
-# B1= blue, B2=verde, B3=rosso, B4=IR, B5=mediumIR, B6=thermal(lontano)IR, B7=NIR
+# B1= blue, B2=verde, B3=rosso, B4=IR, B5=mediumIR, B6=thermal(lontano)IR, B7=SWIR
 #color change
 cl <- colorRampPalette(c("black", "grey", "light grey"))(100)
 # plot again with new color
