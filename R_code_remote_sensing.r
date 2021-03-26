e43066e83364f473fe8ceba53f1a18a76bebe8c1
Agnesotti87 <77006752+Agnesotti87@users.noreply.github.com>
2021-03-26 10:51:38 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -32,3 +32,34 @@ plot(p224r63_2011$B1_sre, col=clb)
 plot(p224r63_2011$B2_sre, col=clg)
 plot(p224r63_2011$B3_sre, col=clr)
 plot(p224r63_2011$B4_sre, col=clnir)
+#DAY3
+#plot in RGB
+plotRGB(p224r63_2011, r=3, g=2, b=1,stretch="Lin")
+#stretch permette di non schiacchiare l'immagine su un colore piuttosto che in un altro, fa vedere meglio le sfumature, trattandosi di un METODO va messo tra virgolette
+#si può abbreviare anche
+plotRGB(p224r63_2011, 3,2,1,stretch="Lin")
+#questa visualizzazione si chiama immagine a colori naturali
+#modifichiamo i colori con cui visualizziamo le bande, montando IR sulla componente red
+plotRGB(p224r63_2011, 4,3,2,stretch="Lin")
+#visulizziamo più risultati
+dev.off()
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1,stretch="Lin")
+plotRGB(p224r63_2011, 4,3,2,stretch="Lin")
+plotRGB(p224r63_2011, 3,4,2,stretch="Lin")
+plotRGB(p224r63_2011, 3,2,4,stretch="Lin")
+#proviamo uno stretch più forte, con Hist e confrontiamo
+dev.off()
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+#par con natural color, false color e stretch hist
+dev.off()
+par(mfrow=c(3,1))
+#natural color
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
+#false color
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+#stretch lin
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
+#TIPS: colorist package permette di giocare con i colori 
